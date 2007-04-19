feb5b6f9169f56ea4f0f4ab83c678de81d88688b
qbert <qbert@6538119e-2221-0410-ba12-9a5a7e5062c7>
2007-04-19 22:23:58 +0000

    git-svn-id: http://svn.dsource.org/projects/descent/trunk@298 6538119e-2221-0410-ba12-9a5a7e5062c7

--- descent.core/src/descent/internal/core/builder/DAntBuilder.java
+++ descent.core/src/descent/internal/core/builder/DAntBuilder.java
@@ -1,7 +1,14 @@
 package descent.internal.core.builder;
 
-import java.util.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.apache.tools.ant.DefaultLogger;
+import org.apache.tools.ant.Project;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceVisitor;
@@ -9,7 +16,6 @@ import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.OperationCanceledException;
 
 import descent.internal.core.util.Util;
 
@@ -31,8 +37,12 @@ public class DAntBuilder extends IncrementalProjectBuilder {
 			
 			IProject project = getProject();
 			DResourceVisitor visitor;
-
+			// for now just delete the file and create
 			if (true ) { //!project.getFile("build.xml").exists()) {
+				
+				IFile buildFile = project.getFile("build.xml");
+				
+				buildFile.delete(true, monitor);
 				visitor = new DResourceVisitor();
 				
 				project.accept(visitor);
@@ -40,15 +50,22 @@ public class DAntBuilder extends IncrementalProjectBuilder {
 				
 				DAntFileCreator dant = new DAntFileCreator(visitor.projectFiles);
 				
-				Util.log(null, dant.create(project));
+				String buildXml = dant.create(project);
+				Util.log(null, buildXml);
+				Util.log(null, args.toString());				
+				buildFile.create(new ByteArrayInputStream(buildXml.getBytes() ) , true, monitor );
 				
+				Project antProject = new Project();
+				antProject.setUserProperty("ant.file",buildFile.getFullPath().toOSString());
 				
+				DefaultLogger log = new DefaultLogger();
+				//log.setOutputPrintStream(
 				
 				
 			}
 			
 			
-
+			forgetLastBuiltState();
 		}
 		
 		return null;
@@ -61,8 +78,14 @@ class DResourceVisitor implements IResourceVisitor {
 
 	public boolean visit(IResource resource) throws CoreException {
 		// TODO Auto-generated method stub
-
-		Util.log(null, resource.getFullPath().toString());
+		if ( resource.getType() == IResource.FILE)
+		{
+			
+			if ( resource.getFileExtension().toLowerCase().equals("d"))
+			{
+				projectFiles.add(resource.getFullPath());			
+			}
+		}
 		return true;
 	}
 
@@ -82,36 +105,7 @@ class DAntFileCreator {
 	}
 
 	public String create(IProject project) {
-		String macrodef = "<macrodef name=\"compile\" >";
-		macrodef += "<attribute name=\"bldtrg\" />";
-		macrodef += "<sequential>";
-		macrodef += "<D ";
-		macrodef += "type        = \"${compiler.type}\"";
-		macrodef += "mode        = \"executable\"";
-		macrodef += "compilerdir = \"${compiler.dir}\"";
-		macrodef += "destfile    = \"${project.dir}/@{bldtrg}.exe\"";
-		macrodef += "cleanup     = \"true\"";
-		macrodef += ">";
-		macrodef += "<!--				<version value=\"Posix\" /> -->";
-		macrodef += "";
-		macrodef += "<!-- The main modules -->";
-		macrodef += "<mainmodules>";
-		macrodef += "<fileset file=\"${project.dir}/@{bldtrg}.d\"/>";
-		macrodef += "</mainmodules>";
-		macrodef += "";
-		macrodef += "<!-- Modules for compilation and linking, if imported -->";
-		macrodef += "<includemodules>";
-		macrodef += "<dirset file=\"${project.dir}\" />";
-		macrodef += "<dirset file=\"${tango.dir}\" />";
-		macrodef += "</includemodules>";
-		macrodef += "";
-		macrodef += "<!-- Imported modules, only for declarations, no compile/link (libs) -->";
-		macrodef += "<includepath>";
-		macrodef += "</includepath>";
-		macrodef += "</D>";
-		macrodef += "</sequential>";
-		macrodef += "</macrodef>";
-
+		
 		String antText = "";
 		String compilerType = "";
 		
@@ -123,33 +117,73 @@ class DAntFileCreator {
 
 		if (isWindows) {
 			compilerType = "dmd-windows";
-
-
 		}
 		else
 		{
 			compilerType = "dmd-linux";
 		}
 		
+		
 
-		antText += "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>";
+
+		
+
+		antText += "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n";
 		antText += "<project name=\"" + project.getName()
-				+ "\" default=\"compile\" basedir=\".\">";
-		antText += "<taskdef classname=\"anttasks.D\" name=\"D\" />";
-		antText += "<taskdef classname=\"anttasks.DModuleBuildNumber\" name=\"DBldNum\" />";
-		antText += "<taskdef classname=\"anttasks.DModuleBuildNumber\" name=\"foreach\" />";
-		antText += "<property name=\"compiler.dir\" value=\"C:\\\" />";
+				+ "\" default=\"compile\" basedir=\".\">\n";
+		antText += "<taskdef classname=\"anttasks.D\" name=\"D\" />\n\n";
+		antText += "<taskdef classname=\"anttasks.DModuleBuildNumber\" name=\"DBldNum\" />\n";
+		antText += "<taskdef classname=\"anttasks.DModuleBuildNumber\" name=\"foreach\" />\n\n";
+		antText += "<property name=\"compiler.dir\" value=\"C:\\\" />\n";
 		antText += "<property name=\"compiler.type\" value=\""
-				+ compilerType + "\"/>";
+				+ compilerType + "\"/>\n";
 
 		antText += "<property name=\"project.dir\" value=\""
-				+ project.getFullPath().toOSString() + "\" />";
-		antText += "<property name=\"tango.dir\" value=\"C:\\path\\to\\tango\"";
+				+ project.getLocation().removeLastSegments(1) + "\" />\n";
+		antText += "<!-- <property name=\"tango.dir\" value=\"C:\\path\\to\\tango\"/> --> \n\n\n";
+		
+		String macrodef = "<macrodef name=\"compile\" >\n";		
+		macrodef += "<sequential>\n";
+		macrodef += "<D \n";
+		macrodef += "type        = \"${compiler.type}\"\n";
+		macrodef += "mode        = \"executable\"\n";
+		macrodef += "compilerdir = \"${compiler.dir}\"\n";
+		macrodef += "destfile    = \"${project.dir}/" + project.getName() + ".exe\"\n";
+		macrodef += "cleanup     = \"true\"\n";
+		macrodef += ">\n";
+		if ( !isWindows ) macrodef += "<version value=\"Posix\" />\n";
+		macrodef += "\n";
+		macrodef += "<!-- The main modules -->\n";
+		macrodef += "<mainmodules>\n";
+		for ( IPath path : files )
+			{
+			macrodef += "<fileset file=\"${project.dir}" + path.toOSString() + "\"/>\n";
+			}
+		macrodef += "</mainmodules>\n";
+		macrodef += "\n";
+		macrodef += "<!-- Modules for compilation and linking, if imported -->\n";
+		macrodef += "<includemodules>\n";
+		macrodef += "<dirset file=\"${project.dir}\" />\n";
+		macrodef += "<dirset file=\"${tango.dir}\" />\n";
+		macrodef += "</includemodules>\n";
+		macrodef += "\n";
+		macrodef += "<!-- Imported modules, only for declarations, no compile/link (libs) -->\n";
+		macrodef += "<includepath>\n";
+		macrodef += "</includepath>\n";
+		macrodef += "</D>\n";
+		macrodef += "</sequential>\n";
+		macrodef += "</macrodef>\n\n\n";
 		
 		antText += macrodef;
 		
 		
-			
+		antText += "\n\n<target name=\"compile\" >\n";
+		antText += "<compile/>\n";
+		antText += "</target>\n";
+		antText += "</project>\n";
+
+	
+	
 
 		return antText;
 	}
