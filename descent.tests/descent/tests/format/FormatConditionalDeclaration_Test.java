155877b12fb698ecfeeda43c7deac0dfe766f1a9
asterite <asterite@6538119e-2221-0410-ba12-9a5a7e5062c7>
2007-06-18 19:36:01 +0000

    Some more tests...
    git-svn-id: http://svn.dsource.org/projects/descent/trunk@439 6538119e-2221-0410-ba12-9a5a7e5062c7

---
+++ descent.tests/descent/tests/format/FormatConditionalDeclaration_Test.java
@@ -0,0 +1,184 @@
+package descent.tests.format;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import descent.core.formatter.DefaultCodeFormatterConstants;
+
+public class FormatConditionalDeclaration_Test extends AbstractFormatter_Test {
+	
+	private final String[] conditionals = { "version", "debug" }; 
+	
+	@Override
+	protected Map getDefaultOptions() {
+		Map options = new HashMap();
+		options.put(DefaultCodeFormatterConstants.FORMATTER_BRACE_POSITION_FOR_CONDITIONAL_DECLARATION, DefaultCodeFormatterConstants.END_OF_LINE);
+		return options;
+	}
+	
+	public void testBracesAtEndOfLine() throws Exception {
+		for(String cond : conditionals) {
+			assertFormat(
+					cond + "(someVersion) {\r\n" +
+					"}", 
+					
+					cond + "    (   someVersion   )  {    }"
+				);
+		}
+	}
+	
+	public void testBracesNextLine() throws Exception {
+		Map options = new HashMap();
+		options.put(DefaultCodeFormatterConstants.FORMATTER_BRACE_POSITION_FOR_CONDITIONAL_DECLARATION, DefaultCodeFormatterConstants.NEXT_LINE);
+		for(String cond : conditionals) {
+			assertFormat(
+					cond + "(someVersion)\r\n" +
+					"{\r\n" +
+					"}", 
+					
+					cond + "    (   someVersion   )  {    }",
+					
+					options
+				);
+		}
+	}
+	
+	public void testBracesNextLineShifted() throws Exception {
+		Map options = new HashMap();
+		options.put(DefaultCodeFormatterConstants.FORMATTER_BRACE_POSITION_FOR_CONDITIONAL_DECLARATION, DefaultCodeFormatterConstants.NEXT_LINE_SHIFTED);
+		for(String cond : conditionals) {
+			assertFormat(
+					cond + "(someVersion)\r\n" +
+					"\t{\r\n" +
+					"\t}", 
+					
+					cond + "    (   someVersion   )  {    }",
+					
+					options
+				);
+		}
+	}
+	
+	public void testWithComments() throws Exception {
+		for(String cond : conditionals) {
+			assertFormat(
+					"/*\r\n" +
+					" * Some\r\n" +
+					" * comment\r\n" +
+					" */\r\n" +
+					cond + "(someVersion) { // comment\r\n" +
+					"}", 
+					
+					"/*\r\n" +
+					" * Some\r\n" +
+					" * comment\r\n" +
+					" */\r\n" +
+					cond + "    (   someVersion   )  { // comment\r\n   }"
+				);
+		}
+	}
+	
+	public void testBracesNextLineShiftedWithMembers() throws Exception {
+		Map options = new HashMap();
+		options.put(DefaultCodeFormatterConstants.FORMATTER_BRACE_POSITION_FOR_CONDITIONAL_DECLARATION, DefaultCodeFormatterConstants.NEXT_LINE_SHIFTED);
+		for(String cond : conditionals) {
+			assertFormat(
+					cond + "(someVersion)\r\n" +
+					"\t{\r\n" +
+					"\t\tint x;\r\n" +
+					"\t}", 
+					
+					cond + "(someVersion) {  int x;  }",
+					
+					options
+				);
+		}
+	}
+	
+	// TODO Descent formatter: make it configurable to write the declaration without an end line
+	public void testSingleDeclaration() throws Exception {
+		for(String cond : conditionals) {
+			assertFormat(
+					cond + "(someVersion)\r\n" +
+						"\tint x;", 
+					
+					cond + "    (   someVersion   )  int   x ;"
+				);
+		}
+	}
+	
+	// TODO Descent formatter: make it configurable to write the "else" without an end line
+	public void testBracesAtEndOfLineWithElse() throws Exception {
+		for(String cond : conditionals) {
+			assertFormat(
+					cond + "(someVersion) {\r\n" +
+					"}\r\n" +
+					"else {\r\n" +
+					"}", 
+					
+					cond + "    (   someVersion   )  {    }  else  {   }"
+				);
+		}
+	}
+	
+	public void testBracesNextLineWithElse() throws Exception {
+		Map options = new HashMap();
+		options.put(DefaultCodeFormatterConstants.FORMATTER_BRACE_POSITION_FOR_CONDITIONAL_DECLARATION, DefaultCodeFormatterConstants.NEXT_LINE);
+		for(String cond : conditionals) {
+			assertFormat(
+					cond + "(someVersion)\r\n" +
+					"{\r\n" +
+					"}\r\n" +
+					"else\r\n" + 
+					"{\r\n" +
+					"}", 
+					
+					cond + "    (   someVersion   )  {    }  else  {   }",
+					
+					options
+				);
+		}
+	}
+	
+	public void testBracesNextLineShiftedWithElse() throws Exception {
+		Map options = new HashMap();
+		options.put(DefaultCodeFormatterConstants.FORMATTER_BRACE_POSITION_FOR_CONDITIONAL_DECLARATION, DefaultCodeFormatterConstants.NEXT_LINE_SHIFTED);
+		for(String cond : conditionals) {
+			assertFormat(
+					cond + "(someVersion)\r\n" +
+					"\t{\r\n" +
+					"\t}\r\n" +
+					"else\r\n" +
+					"\t{\r\n" +
+					"\t}",
+					
+					cond + "    (   someVersion   )  {    }  else  {   }",
+					
+					options
+				);
+		}
+	}
+	
+	public void testBracesNextLineShiftedWithMembersWithElse() throws Exception {
+		Map options = new HashMap();
+		options.put(DefaultCodeFormatterConstants.FORMATTER_BRACE_POSITION_FOR_CONDITIONAL_DECLARATION, DefaultCodeFormatterConstants.NEXT_LINE_SHIFTED);
+		for(String cond : conditionals) {
+			assertFormat(
+					cond + "(someVersion)\r\n" +
+					"\t{\r\n" +
+					"\t\tint x;\r\n" +
+					"\t}\r\n" +
+					"else\r\n" +
+					"\t{\r\n" +
+					"\t\tfloat x;\r\n" +
+					"\t}", 
+					
+					cond + "(someVersion) {  int x;  }  else   {   float  x ; }",
+					
+					options
+				);
+		}
+	}
+	
+
+}
---
+++ descent.tests/descent/tests/format/FormatStatements_Test.java
@@ -0,0 +1,18 @@
+package descent.tests.format;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FormatStatements_Test extends AbstractFormatter_Test {
+
+	@Override
+	protected Map getDefaultOptions() {
+		Map options = new HashMap();
+		return options;
+	}
+	
+	public void testDummy() {
+		
+	}
+
+}
