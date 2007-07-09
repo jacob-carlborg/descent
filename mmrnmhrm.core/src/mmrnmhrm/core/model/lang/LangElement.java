ac84cc90008cd5f936957c42843b281d47000ee8
phoenix <phoenix@6538119e-2221-0410-ba12-9a5a7e5062c7>
2007-07-09 16:08:54 +0000

    Refactoring and minor fixes.
    git-svn-id: http://svn.dsource.org/projects/descent/trunk@503 6538119e-2221-0410-ba12-9a5a7e5062c7

---
+++ mmrnmhrm.core/src/mmrnmhrm/core/model/lang/LangElement.java
@@ -0,0 +1,84 @@
+package mmrnmhrm.core.model.lang;
+
+import java.util.ArrayList;
+
+import melnorme.miscutil.Assert;
+import melnorme.miscutil.tree.IElement;
+
+
+
+/**
+ * Common class for IDE language model elements.
+ * Not handle-based nor cached, like JDT.
+ */
+public abstract class LangElement extends LangElementInfo implements ILangElement {
+	
+	protected ILangElement parent;
+	protected boolean opened = false;
+	
+	public LangElement(ILangElement parent) {
+		this.parent = parent;
+	}
+	
+	public ILangElement getParent() {
+		return parent;
+	}
+	
+	public boolean hasChildren() {
+		return getChildren().length > 0;
+	}
+
+	
+	/** Returns true if this handle represents the same Lang element
+	 * as the given handle. By default, two handles represent the same
+	 * element if they are identical or if they represent the same type
+	 * of element, have equal names, parents, and occurrence counts.
+	 * 
+	 * XXX: occurrence counts ?
+	 */
+	public boolean equals(Object obj) {
+		
+		if (this == obj) return true;
+	
+		// Only lang model root's parent is null
+		if (this.parent == null) 
+			return (this == obj); // model root is a singleton
+
+		Assert.isNotNull(this.parent);
+
+		if(!(obj instanceof LangElement))
+			return false;
+
+		LangElement other = (LangElement) obj;		
+		return getElementName().equals(other.getElementName()) &&
+				this.parent.equals(other.parent);
+	}
+	
+	/** Returns the hash code for this Lang element. By default,
+	 * the hash code for an element is a combination of its name
+	 * and parent's hash code. Elements with other requirements must
+	 * override this method. 
+	 */
+	public int hashCode() {
+		if (this.parent == null) 
+			return super.hashCode();
+		return melnorme.miscutil.Misc.combineHashCodes(getElementName().hashCode(), this.parent.hashCode());
+	}
+	
+	/** Returns a collection of (immediate) children of this node of the
+	 * specified element type.
+	 */
+	public ArrayList<?> getChildrenOfType(int type) {
+		IElement[] children = getChildren();
+		int size = children.length;
+		ArrayList<ILangElement> list = new ArrayList<ILangElement>(size);
+		for (int i = 0; i < size; ++i) {
+			LangElement elt = (LangElement)children[i];
+			if (elt.getElementType() == type) {
+				list.add(elt);
+			}
+		}
+		return list;
+	}
+	
+}
