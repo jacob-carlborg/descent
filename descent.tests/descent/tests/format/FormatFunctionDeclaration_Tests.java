d23437f6da9e78c2e6e3e2b43ef91e16344cd213
fraserofthenight <fraserofthenight@6538119e-2221-0410-ba12-9a5a7e5062c7>
2007-07-10 22:19:13 +0000

    More formatter work... this is truly never-ending...
    git-svn-id: http://svn.dsource.org/projects/descent/trunk@506 6538119e-2221-0410-ba12-9a5a7e5062c7

--- descent.tests/descent/tests/format/FormatFunctionDeclaration_Tests.java
+++ descent.tests/descent/tests/format/FormatFunctionDeclaration_Tests.java
@@ -13,7 +13,7 @@ public class FormatFunctionDeclaration_Tests extends AbstractFormatter_Test {
 	protected Map getDefaultOptions() {
 		Map options = new HashMap();
 		options.put(DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_BEFORE_SEMICOLON, DefaultCodeFormatterConstants.FALSE);
-		options.put(DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_BEFORE_OPENING_PAREN_IN_FUNCTION_DECLARATION, DefaultCodeFormatterConstants.FALSE);
+		options.put(DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_BEFORE_OPENING_PAREN_IN_FUNCTION_DECLARATION_PARAMETERS, DefaultCodeFormatterConstants.FALSE);
 		options.put(DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_AFTER_OPENING_PAREN_IN_FUNCTION_DECLARATION, DefaultCodeFormatterConstants.FALSE);
 		options.put(DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_BEFORE_CLOSING_PAREN_IN_FUNCTION_DECLARATION, DefaultCodeFormatterConstants.FALSE);
 		options.put(DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_BETWEEN_EMPTY_PARENS_IN_FUNCTION_DECLARATION, DefaultCodeFormatterConstants.FALSE);
@@ -54,7 +54,7 @@ public class FormatFunctionDeclaration_Tests extends AbstractFormatter_Test {
 	
 	public void testEmptyInsertSpaceBeforeOpenParen() throws Exception {
 		Map options = new HashMap();
-		options.put(DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_BEFORE_OPENING_PAREN_IN_FUNCTION_DECLARATION, DefaultCodeFormatterConstants.TRUE);
+		options.put(DefaultCodeFormatterConstants.FORMATTER_INSERT_SPACE_BEFORE_OPENING_PAREN_IN_FUNCTION_DECLARATION_PARAMETERS, DefaultCodeFormatterConstants.TRUE);
 		for(String prefix : prefixes) {
 			assertFormat(
 					prefix + " ();", 
