4f61f87e2e21a34b65344aa7ed98ccad713e237c
asterite <asterite@6538119e-2221-0410-ba12-9a5a7e5062c7>
2007-08-09 11:33:19 +0000

    Started the trace viewer...
    git-svn-id: http://svn.dsource.org/projects/descent/trunk@617 6538119e-2221-0410-ba12-9a5a7e5062c7

---
+++ descent.ui/src/descent/internal/ui/traceeditor/TraceEditor.java
@@ -0,0 +1,42 @@
+package descent.internal.ui.traceeditor;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.part.EditorPart;
+
+public class TraceEditor extends EditorPart {
+
+	@Override
+	public void doSave(IProgressMonitor monitor) {
+	}
+
+	@Override
+	public void doSaveAs() {
+	}
+
+	@Override
+	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
+	}
+
+	@Override
+	public boolean isDirty() {
+		return false;
+	}
+
+	@Override
+	public boolean isSaveAsAllowed() {
+		return false;
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+	}
+
+	@Override
+	public void setFocus() {
+	}
+
+}
