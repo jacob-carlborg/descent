9dfb37975273f0e230b89c3d50f9bf6acf44ad97
asterite <asterite@6538119e-2221-0410-ba12-9a5a7e5062c7>
2007-08-11 17:28:33 +0000

    Added filtering and sorting capabilities to the trace.log viewer.
    git-svn-id: http://svn.dsource.org/projects/descent/trunk@623 6538119e-2221-0410-ba12-9a5a7e5062c7

--- descent.ui/src/descent/internal/ui/traceeditor/TraceEditor.java
+++ descent.ui/src/descent/internal/ui/traceeditor/TraceEditor.java
@@ -9,10 +9,20 @@ import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeColumn;
 import org.eclipse.ui.IEditorInput;
@@ -31,14 +41,14 @@ import descent.ui.JavaUI;
 public class TraceEditor extends EditorPart {
 	
 	private final static int COLUMN_FUNCTION_NAME = 0;
-	private final static int COLUMN_NUMBER_OF_CALLS = 1;
+	private final static int COLUMN_NUMBER_OF_CALLS = 1;	
 	private final static int COLUMN_TREE_TIME = 2;
 	private final static int COLUMN_FUNCTION_TIME = 3;
 	private final static int COLUMN_FUNCTION_TIME_PER_CALL = 4;
 	private final static int COLUMN_TICKS = 5;
 	private final static int COLUMN_TREE_TICKS = 6;
 	
-	class TraceContentProvider implements ITreeContentProvider {
+	private class TraceContentProvider implements ITreeContentProvider {
 
 		public Object[] getChildren(Object element) {
 			if (element instanceof ITraceNode) {
@@ -49,6 +59,8 @@ public class TraceEditor extends EditorPart {
 				System.arraycopy(fanIn, 0, children, 0, fanIn.length);
 				System.arraycopy(fanOut, 0, children, fanIn.length, fanOut.length);
 				return children;
+			} else if (element instanceof IFan) {
+				return getChildren(((IFan) element).getTraceNode());
 			}
 			return null;
 		}
@@ -61,6 +73,8 @@ public class TraceEditor extends EditorPart {
 			if (element instanceof ITraceNode) {
 				ITraceNode node = (ITraceNode) element;
 				return node.getFanIn().length + node.getFanOut().length > 0;
+			} else if (element instanceof IFan) {
+				return hasChildren(((IFan) element).getTraceNode());
 			}
 			return false;
 		}
@@ -77,7 +91,7 @@ public class TraceEditor extends EditorPart {
 		
 	}
 	
-	class TraceLabelProvider implements ITableLabelProvider {
+	private class TraceLabelProvider implements ITableLabelProvider {
 
 		public Image getColumnImage(Object element, int columnIndex) {
 			if (element instanceof ITraceNode) {
@@ -125,17 +139,17 @@ public class TraceEditor extends EditorPart {
 				case COLUMN_FUNCTION_NAME:
 					 return node.getDemangledName();
 				case COLUMN_FUNCTION_TIME:
-					return "-";
+					return String.valueOf(node.getFunctionTime());
 				case COLUMN_FUNCTION_TIME_PER_CALL:
-					return "-";
+					return String.valueOf(node.getFunctionTimePerCall());
 				case COLUMN_NUMBER_OF_CALLS:
-					return String.valueOf(fan.getNumberOfCalls());
+					return fan.getNumberOfCalls() + " / " + node.getNumberOfCalls(); //$NON-NLS-1$
 				case COLUMN_TICKS:
-					return "-";
+					return String.valueOf(node.getTicks());
 				case COLUMN_TREE_TICKS:
-					return "-";
+					return String.valueOf(node.getTreeTicks());
 				case COLUMN_TREE_TIME:
-					return "-";
+					return String.valueOf(node.getTreeTime());
 				}
 			}
 			return null;
@@ -156,8 +170,78 @@ public class TraceEditor extends EditorPart {
 		
 	}
 	
+	private class TraceSorter extends ViewerSorter {
+		
+		@Override
+		public int compare(Viewer viewer, Object e1, Object e2) {
+			final Tree tree = ((TreeViewer) viewer).getTree();
+			final TreeColumn sortCol = tree.getSortColumn();
+			final int sortDirection = tree.getSortDirection();
+
+			if (sortCol != null && 
+					e1 instanceof ITraceNode && 
+					e2 instanceof ITraceNode) {
+				ITraceNode t1 = (ITraceNode) e1;
+				ITraceNode t2 = (ITraceNode) e2;
+				int columnIndex = tree.indexOf(sortCol);
+				int result = 0;
+				switch (columnIndex) {
+				case COLUMN_FUNCTION_NAME:
+					result = t1.getDemangledName().compareTo(t2.getDemangledName());
+					break;
+				case COLUMN_FUNCTION_TIME:
+					result = compareLongs(t1.getFunctionTime(), t2.getFunctionTime());
+					break;
+				case COLUMN_FUNCTION_TIME_PER_CALL:
+					result = compareLongs(t1.getFunctionTimePerCall(), t2.getFunctionTimePerCall());
+					break;
+				case COLUMN_NUMBER_OF_CALLS:
+					result = compareLongs(t1.getNumberOfCalls(), t2.getNumberOfCalls());
+					break;
+				case COLUMN_TICKS:
+					result = compareLongs(t1.getTicks(), t2.getTicks());
+					break;
+				case COLUMN_TREE_TICKS:
+					result = compareLongs(t1.getTreeTicks(), t2.getTreeTicks());
+					break;
+				case COLUMN_TREE_TIME:
+					result = compareLongs(t1.getTreeTime(), t2.getTreeTime());
+					break;
+				}
+				if (sortDirection == SWT.DOWN) {
+					return -result;
+				} else {
+					return result;
+				}
+			}
+			return super.compare(viewer, e1, e2);
+		}
+		
+		private int compareLongs(long e1, long e2) {
+			return e1 > e2 ? 1 : (e1 < e2 ? -1 : 0);
+		}
+		
+	}
+	
+	private class TraceFilter extends ViewerFilter {
+
+		@Override
+		public boolean select(Viewer viewer, Object parentElement, Object element) {
+			if (!(parentElement instanceof ITrace) || 
+					!(element instanceof ITraceNode) ||
+					filterText == null) {
+				return true;
+			}
+			
+			ITraceNode node = (ITraceNode) element;
+			return node.getDemangledName().contains(filterText);
+		}
+		
+	}
+	
 	private TreeViewer viewer;
 	private ITrace trace;
+	private String filterText;
 
 	@Override
 	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
@@ -182,29 +266,93 @@ public class TraceEditor extends EditorPart {
 	@SuppressWarnings("deprecation")
 	@Override
 	public void createPartControl(Composite parent) {
-		viewer = new TreeViewer(parent);
-		viewer.getTree().setLayoutData(new GridData(GridData.FILL_BOTH));
+		Group group = new Group(parent, SWT.NONE);
+		group.setLayout(new GridLayout(2, false));
 		
-	    Tree tree = viewer.getTree();
-	    
-	    new TreeColumn(tree, SWT.LEFT).setText("Function");
-	    new TreeColumn(tree, SWT.RIGHT).setText("Number of calls");
-	    new TreeColumn(tree, SWT.RIGHT).setText("Tree time");
-	    new TreeColumn(tree, SWT.RIGHT).setText("Function time");
-	    new TreeColumn(tree, SWT.RIGHT).setText("Function time per call");
-	    new TreeColumn(tree, SWT.RIGHT).setText("Ticks");
-	    new TreeColumn(tree, SWT.RIGHT).setText("Tree ticks");
-	    
-	    for (int i = 0, n = tree.getColumnCount(); i < n; i++) {
-	    	tree.getColumn(i).pack();
-	    }
+		Label label = new Label(group, SWT.NONE);
+		label.setText("Filter:");
+		label.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1));
+		
+		final Text text = new Text(group, SWT.BORDER);
+		text.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false, 1, 1));
+		text.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				filterText = text.getText();
+				viewer.refresh();
+			}
+		});
+		
+		Tree tree = createTreeViewer(group);
+		tree.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+		
+		label = new Label(group, SWT.NONE);
+		label.setText("Timer Is " + trace.getTicksPerSecond() + " ticks/sec, times are in microsecs");
+		label.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1));
+	}
+	
+	private Tree createTreeViewer(Composite parent) {
+		viewer = new TreeViewer(parent);		
+	    final Tree tree = viewer.getTree();
 	    
 	    tree.setHeaderVisible(true);
 	    tree.setLinesVisible(true);
 	    
+	    TreeColumn col = new TreeColumn(tree, SWT.LEFT);
+		col.setText("Function");
+		col.setWidth(300);
+		
+	    col = new TreeColumn(tree, SWT.RIGHT);
+	    col.setText("Number of calls (fan / total)");
+	    col.setWidth(140);
+	    
+	    col = new TreeColumn(tree, SWT.RIGHT);
+	    col.setText("Tree time");
+	    col.setWidth(60);
+	    
+	    col = new TreeColumn(tree, SWT.RIGHT);
+	    col.setText("Function time");
+	    col.setWidth(80);
+	    
+	    col = new TreeColumn(tree, SWT.RIGHT);
+	    col.setText("Function time per call");
+	    col.setWidth(120);
+	    
+	    col = new TreeColumn(tree, SWT.RIGHT);
+	    col.setText("Ticks");
+	    col.setWidth(60);
+	    
+	    col = new TreeColumn(tree, SWT.RIGHT);
+	    col.setText("Tree ticks");
+	    col.setWidth(60);
+	    
+	    Listener sortListener = new Listener() {
+	        public void handleEvent(Event event) {
+	        	 final Tree tree = viewer.getTree();
+				final TreeColumn sortColumn = tree.getSortColumn();
+				final TreeColumn currentColumn = (TreeColumn) event.widget;
+				int dir = SWT.UP;
+				if (sortColumn == currentColumn) {
+					dir = (tree.getSortDirection() == SWT.UP) ? SWT.DOWN
+							: SWT.UP;
+				} else {
+					tree.setSortColumn(currentColumn);
+				}
+				tree.setSortDirection(dir);
+				viewer.refresh();
+	        }
+	    };
+	    
+	    for(TreeColumn column : tree.getColumns()) {
+	    	column.addListener(SWT.Selection, sortListener);
+	    }
+	    
 	    viewer.setContentProvider(new TraceContentProvider());
 		viewer.setLabelProvider(new TraceLabelProvider());
+		viewer.setSorter(new TraceSorter());
+		viewer.addFilter(new TraceFilter());
 		viewer.setInput(trace);
+		
+		return tree;
 	}
 
 	@Override
