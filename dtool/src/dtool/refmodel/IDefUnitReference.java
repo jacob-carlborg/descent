e57b6a4f91b0d74571ba384f7ac1a2eed96b8b52
phoenix <phoenix@6538119e-2221-0410-ba12-9a5a7e5062c7>
2007-08-17 18:02:34 +0000

    Mmrnmhrm release 0.1: Parser conversion is fully finished, now Phobos and Tango compile. Fixed several bugs.
    git-svn-id: http://svn.dsource.org/projects/descent/trunk@645 6538119e-2221-0410-ba12-9a5a7e5062c7

---
+++ dtool/src/dtool/refmodel/IDefUnitReference.java
@@ -0,0 +1,14 @@
+package dtool.refmodel;
+
+import java.util.Collection;
+
+import dtool.dom.definitions.DefUnit;
+
+/** A references to a DefUnit. */
+public interface IDefUnitReference {
+
+	/** Finds the DefUnits matching this reference. 
+	 * Result can be null if none found.
+	 * XXX: make it allways null for no results? */
+	Collection<DefUnit> findTargetDefUnits(boolean findFirstOnly);
+}
