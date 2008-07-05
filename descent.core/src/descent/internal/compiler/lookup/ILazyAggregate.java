ed604013af2ae6095474976a2b345945c7a423c8
asterite <asterite@6538119e-2221-0410-ba12-9a5a7e5062c7>
2008-07-05 14:18:05 +0000

    Oops, forgot this file
    git-svn-id: http://svn.dsource.org/projects/descent/trunk@1208 6538119e-2221-0410-ba12-9a5a7e5062c7

---
+++ descent.core/src/descent/internal/compiler/lookup/ILazyAggregate.java
@@ -0,0 +1,33 @@
+package descent.internal.compiler.lookup;
+
+import descent.core.IParent;
+import descent.internal.compiler.parser.Dsymbol;
+import descent.internal.compiler.parser.DsymbolTable;
+import descent.internal.compiler.parser.Dsymbols;
+import descent.internal.compiler.parser.Loc;
+import descent.internal.compiler.parser.Scope;
+import descent.internal.compiler.parser.SemanticContext;
+
+public interface ILazyAggregate extends ILazy {
+
+	Dsymbol super_search(Loc loc, char[] ident, int flags, SemanticContext context);
+
+	DsymbolTable symtab();
+
+	ModuleBuilder builder();
+
+	IParent getJavaElement();
+
+	void members(Dsymbols dsymbols);
+
+	Dsymbols members();
+
+	Scope semanticScope();
+	
+	Scope semantic2Scope();
+	
+	Scope semantic3Scope();
+
+	void symtab(DsymbolTable table);
+
+}
