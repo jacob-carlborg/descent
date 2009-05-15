eec611570e91dd99a2dbba970ef95c0f9054a1a2
fraserofthenight <fraserofthenight@6538119e-2221-0410-ba12-9a5a7e5062c7>
2009-05-15 09:49:42 +0000

    Line links!!!
    git-svn-id: http://svn.dsource.org/projects/descent/trunk@1340 6538119e-2221-0410-ba12-9a5a7e5062c7

---
+++ descent.debug.ui/src/descent/internal/debug/ui/console/DmdfeConsoleLineTracker.java
@@ -0,0 +1,96 @@
+package descent.internal.debug.ui.console;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.debug.ui.console.IConsole;
+import org.eclipse.debug.ui.console.IConsoleLineTracker;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.ui.console.IHyperlink;
+import org.eclipse.ui.console.IPatternMatchListener;
+import org.eclipse.ui.console.PatternMatchEvent;
+import org.eclipse.ui.console.TextConsole;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+import descent.internal.ui.javaeditor.EditorUtility;
+
+public class DmdfeConsoleLineTracker implements IConsoleLineTracker
+{
+	private static final class DmdfeErrorMatchListener implements IPatternMatchListener
+	{
+		private static final String ERROR_REGEX = ".*?\\(\\d+\\):";
+		private static final String SPLITTER_REGEX = "(.*?)\\((\\d+)\\)";
+		private static Pattern splitterPattern = Pattern.compile(SPLITTER_REGEX);
+		
+		private TextConsole console;
+		private IDocument doc;
+		
+		public void connect(TextConsole console) { this.console = console; this.doc = console.getDocument(); }
+		public void disconnect() { }
+		public String getLineQualifier() { return null; } // PERHAPS
+		public int getCompilerFlags() { return 0; }
+		public String getPattern() { return ERROR_REGEX; }
+
+		public void matchFound(PatternMatchEvent event)
+		{
+			try
+			{
+				int linkOfs = event.getOffset();
+				int linkLength = event.getLength() - 1;
+				String text = doc.get(linkOfs, linkLength);
+				Matcher split = splitterPattern.matcher(text);
+				if(!split.matches() || split.groupCount() != 2)
+					return;
+				String file = split.group(1);
+				int line = Integer.parseInt(split.group(2));
+				console.addHyperlink(new DLocationHyperlink(file, line), linkOfs, linkLength);
+			}
+			catch(Exception e)
+			{
+				// TODO
+			}
+		}
+	}
+	
+	private static final class DLocationHyperlink implements IHyperlink
+	{
+		private final String filename;
+		private final int line;
+		
+		public DLocationHyperlink(String filename, int line)
+		{
+			this.filename = filename;
+			this.line = line;
+		}
+		
+		public void linkEntered() { }
+		public void linkExited() { }
+		
+		public void linkActivated()
+		{	
+			try
+			{
+				IFile file = ResourceSearch.search(filename);
+				if(null == file)
+					return;
+				ITextEditor editor = (ITextEditor) EditorUtility.openInEditor(file);
+				if(null == editor)
+					return;
+				IDocument document = editor.getDocumentProvider().getDocument(editor.getEditorInput());
+				if(null == document)
+					return;
+				editor.selectAndReveal(document.getLineOffset(line - 1), document.getLineLength(line - 1));
+			}
+			catch (Exception e)
+			{
+				// TODO
+			}
+		}
+	}
+	
+	public void init(IConsole console) { console.addPatternMatchListener(new DmdfeErrorMatchListener()); }
+	public void dispose() { }
+	public void lineAppended(IRegion line) { }
+}
---
+++ descent.debug.ui/src/descent/internal/debug/ui/console/ResourceSearch.java
@@ -0,0 +1,123 @@
+package descent.internal.debug.ui.console;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+
+import descent.core.ICompilationUnit;
+import descent.core.IJavaElement;
+import descent.core.IJavaProject;
+import descent.core.IPackageFragment;
+import descent.core.IPackageFragmentRoot;
+import descent.core.JavaCore;
+import descent.core.JavaModelException;
+
+/*package*/ class ResourceSearch
+{
+	public static IFile search(String filename)
+	{
+		// TODO -- REALLY stupid algorithm. Every time you click, it recreates a ternary tree
+		// of the entire workspace, and then does prefix searches of the path with the beginnings
+		// chopped off until it finds the file
+		IPath path  = new Path(filename);
+		try
+		{
+			TernaryTree<IFile> tree = new TernaryTree<IFile>();
+			enumerateProjects(ResourcesPlugin.getWorkspace().getRoot().members(), tree);
+			for(IFile file : tree)
+				System.out.println(file);
+			while(path.segmentCount() > 0)
+			{
+				String prefix = reverse(path.toPortableString());
+				// TODO what to do about multiple matches?
+				for(IFile result : tree.prefixSearch(prefix))
+					return result;
+				path = path.removeFirstSegments(1);
+			}
+			return null;
+		}
+		catch(CoreException e)
+		{
+			return null;
+		}
+	}
+	
+	private static void enumerateProjects(IResource[] resources, TernaryTree<IFile> tree) throws CoreException
+	{
+		for(IResource res : resources)
+		{
+			if(res instanceof IProject)
+			{
+				IProject proj = ((IProject) res).getProject();
+				if(!proj.isOpen())
+					continue;
+				
+				// Make sure it's a D project
+				IJavaProject javaProj;
+				try
+				{
+					javaProj = (IJavaProject) proj.getNature(JavaCore.NATURE_ID);
+				}
+				catch(Exception e)
+				{
+					continue;
+				}
+				enumeratePFRs(javaProj.getPackageFragmentRoots(), tree);
+			}
+		}
+	}
+	
+	private static void enumeratePFRs(IPackageFragmentRoot[] elements, TernaryTree<IFile> tree)
+	{
+		for(IPackageFragmentRoot root : elements)
+		{
+			try
+			{
+				enumeratePackageFragments(root.getChildren(), tree);
+			}
+			catch(JavaModelException e)
+			{
+				continue;
+			}
+		}
+	}
+	
+	
+	
+	private static void enumeratePackageFragments(IJavaElement[] elements, TernaryTree<IFile> tree)
+	{
+		for(IJavaElement fragment : elements)
+		{
+			try
+			{
+				if(fragment instanceof IPackageFragment)
+					enumerateCompilationUnits(((IPackageFragment) fragment).getCompilationUnits(), tree);
+			}
+			catch(JavaModelException e)
+			{
+				continue;
+			}
+		}
+	}
+
+	private static void enumerateCompilationUnits(ICompilationUnit[] modules, TernaryTree<IFile> tree)
+	{
+		for(ICompilationUnit module : modules)
+		{
+			IResource res = module.getResource();
+			if(null == res || !(res instanceof IFile))
+				continue;
+			IFile file = (IFile) res;
+			tree.add(reverse(file.getFullPath().toPortableString()), file);
+		}
+	}
+
+	private static String reverse(String s)
+	{
+		return ((new StringBuffer(s)).reverse()).toString();
+	}
+}
