c62e817271ce8433e4923432b6d25f0cfd15254e
fraserofthenight <fraserofthenight@6538119e-2221-0410-ba12-9a5a7e5062c7>
2009-05-28 23:41:19 +0000

    Added UI support for stack stack trace linking
    git-svn-id: http://svn.dsource.org/projects/descent/trunk@1378 6538119e-2221-0410-ba12-9a5a7e5062c7

--- descent.debug.ui/src/descent/internal/debug/ui/MainTab.java
+++ descent.debug.ui/src/descent/internal/debug/ui/MainTab.java
@@ -25,6 +25,7 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.dialogs.ElementListSelectionDialog;
@@ -50,6 +51,11 @@ public class MainTab extends AbstractLaunchConfigurationTab {
 	// Main class UI widgets
 	protected Label fProgLabel;
 	protected Text fProgText;
+	private Button fLineTrackCheckbox;
+	private Label fLineTrackHelp;
+	private Label fLineTrackLabel;
+	private Text fLineTrackText;
+	private Button fLineTrackDefaultButton;
 	
 	protected static final String EMPTY_STRING = ""; //$NON-NLS-1$
 	private final String fMode;
@@ -70,6 +76,7 @@ public class MainTab extends AbstractLaunchConfigurationTab {
 		createVerticalSpacer(comp, 1);
 		createProjectGroup(comp, 1);
 		createExeFileGroup(comp, 1);
+		createLineTrackGroup(comp, 1);
 		createVerticalSpacer(comp, 1);
 		//LaunchUIPlugin.setDialogShell(parent.getShell());
 	}
@@ -147,6 +154,88 @@ public class MainTab extends AbstractLaunchConfigurationTab {
 		});
 	}
 	
+	protected void createLineTrackGroup(Composite parent, int colSpan)
+	{
+		// Create a composite to arrange the options
+		Group comp = new Group(parent, SWT.NONE);
+		comp.setText("Stack trace linking");
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 3;
+		comp.setLayout(layout);
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		gd.horizontalSpan = colSpan;
+		comp.setLayoutData(gd);
+		
+		// Create the help text
+		fLineTrackHelp = new Label(comp, SWT.LEFT | SWT.WRAP);
+		fLineTrackHelp.setText("Descent can be configured to create links to files/lines from stack " +
+				"traces in the console output. To enable this, specify a regular expression to match lines containing a trace " +
+				"element. If there is a match, the first group must match the filename, while the second matches " +
+				"the line. The regular expression syntax is that of java.util.Pattern. The default regular " +
+				"expression matches lines from Tango's stack trace.");
+	    gd = new GridData(GridData.FILL_BOTH);
+	    gd.horizontalSpan = 3;
+	    fLineTrackHelp.setLayoutData(gd);
+		
+		// Create the checkbox
+		fLineTrackCheckbox = new Button(comp, SWT.CHECK);
+		fLineTrackCheckbox.setText("Enable stack trace linking");
+		fLineTrackCheckbox.addSelectionListener(new SelectionAdapter()
+        {
+            public void widgetSelected(SelectionEvent e)
+            {
+                updateLaunchConfigurationDialog();
+                updateLineTrackEnablement();
+            }
+        });
+        gd = new GridData();
+        gd.horizontalSpan = 3;
+        fLineTrackCheckbox.setLayoutData(gd);
+        
+		// Create the label
+		fLineTrackLabel = new Label(comp, SWT.NONE);
+		fLineTrackLabel.setText("Stack trace regex:"); //$NON-NLS-1$
+		gd = new GridData();
+		gd.horizontalSpan = 1;
+		fLineTrackLabel.setLayoutData(gd);
+		
+		// Create the text box
+		fLineTrackText = new Text(comp, SWT.SINGLE | SWT.BORDER);
+		fLineTrackText.addModifyListener(new ModifyListener()
+		{
+			public void modifyText(ModifyEvent evt)
+			{
+				updateLaunchConfigurationDialog();
+			}
+		});
+		gd = new GridData(GridData.FILL_HORIZONTAL);
+        gd.horizontalSpan = 1;
+		fLineTrackText.setLayoutData(gd);
+		
+		// Create the default button
+		fLineTrackDefaultButton = new Button(comp, SWT.PUSH);
+		fLineTrackDefaultButton.setText("Default");
+		fLineTrackDefaultButton.addSelectionListener(new SelectionAdapter()
+        {
+            public void widgetSelected(SelectionEvent e)
+            {
+            	fLineTrackText.setText(IDescentLaunchConfigurationConstants.DEFAULT_STACKTRACE_REGEX);
+                updateLaunchConfigurationDialog();
+            }
+        });
+        gd = new GridData();
+        gd.horizontalSpan = 1;
+        fLineTrackDefaultButton.setLayoutData(gd);
+	}
+	
+	private void updateLineTrackEnablement()
+	{
+		boolean enabled = fLineTrackCheckbox.getSelection();
+		fLineTrackLabel.setEnabled(enabled);
+		fLineTrackText.setEnabled(enabled);
+		fLineTrackDefaultButton.setEnabled(enabled);
+	}
+	
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -155,6 +244,7 @@ public class MainTab extends AbstractLaunchConfigurationTab {
 	public void initializeFrom(ILaunchConfiguration config) {
 		updateProjectFromConfig(config);
 		updateProgramFromConfig(config);
+		updateLineTrackFromConfig(config);
 	}
 	
 	protected void updateProjectFromConfig(ILaunchConfiguration config) {
@@ -177,9 +267,33 @@ public class MainTab extends AbstractLaunchConfigurationTab {
 		fProgText.setText(programName);
 	}
 	
+	protected void updateLineTrackFromConfig(ILaunchConfiguration config)
+	{
+		boolean enabled = false;
+		try {
+			enabled = config.getAttribute(IDescentLaunchConfigurationConstants.ATTR_LINK_STACKTRACE, false);
+		} catch (CoreException ce) {
+			DescentDebugUI.log(ce);
+		}
+		fLineTrackCheckbox.setSelection(enabled);
+		
+		final String dflt = IDescentLaunchConfigurationConstants.DEFAULT_STACKTRACE_REGEX;
+		String regex = dflt;
+		try {
+			regex = config.getAttribute(IDescentLaunchConfigurationConstants.ATTR_STACKTRACE_REGEX, dflt);
+		} catch (CoreException ce) {
+			DescentDebugUI.log(ce);
+		}
+		fLineTrackText.setText(regex);
+		
+		updateLineTrackEnablement();
+	}
+	
 	public void performApply(ILaunchConfigurationWorkingCopy config) {
 		config.setAttribute(IDescentLaunchConfigurationConstants.ATTR_PROJECT_NAME, fProjText.getText());
 		config.setAttribute(IDescentLaunchConfigurationConstants.ATTR_PROGRAM_NAME, fProgText.getText());
+		config.setAttribute(IDescentLaunchConfigurationConstants.ATTR_LINK_STACKTRACE, fLineTrackCheckbox.getSelection());
+		config.setAttribute(IDescentLaunchConfigurationConstants.ATTR_STACKTRACE_REGEX, fLineTrackText.getText());
 	}
 
 	/**
@@ -360,6 +474,10 @@ public class MainTab extends AbstractLaunchConfigurationTab {
 		}
 		
 		config.setAttribute(IDescentLaunchConfigurationConstants.ATTR_PROGRAM_NAME, EMPTY_STRING);
+		
+		config.setAttribute(IDescentLaunchConfigurationConstants.ATTR_LINK_STACKTRACE, false);
+		config.setAttribute(IDescentLaunchConfigurationConstants.DEFAULT_STACKTRACE_REGEX,
+				IDescentLaunchConfigurationConstants.DEFAULT_STACKTRACE_REGEX);
 	}
 
 	public String getName() {
