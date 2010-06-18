f09f15a0020a3e04cecdeb54f49b059d8a5305f2
phoenix <phoenix@6538119e-2221-0410-ba12-9a5a7e5062c7>
2010-06-18 12:50:27 +0000

    HTMLPrinter code extraction
    git-svn-id: http://svn.dsource.org/projects/descent/trunk@1618 6538119e-2221-0410-ba12-9a5a7e5062c7

--- descent.ui/src/descent/internal/ui/text/HTMLPrinter.java
+++ descent.ui/src/descent/internal/ui/text/HTMLPrinter.java
@@ -11,8 +11,6 @@
 package descent.internal.ui.text;
 
 
-import java.io.IOException;
-import java.io.Reader;
 import java.net.URL;
 
 import org.eclipse.swt.SWT;
@@ -20,11 +18,13 @@ import org.eclipse.swt.SWTError;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Display;
 
+import descent.core.ddoc.HTMLPrinterUtils;
+
 
 /**
  * Provides a set of convenience methods for creating HTML pages.
  */
-public class HTMLPrinter {
+public class HTMLPrinter extends HTMLPrinterUtils {
 
 	private static RGB BG_COLOR_RGB= null;
 
@@ -50,52 +50,7 @@ public class HTMLPrinter {
 
 	private HTMLPrinter() {
 	}
-
-	private static String replace(String text, char c, String s) {
-
-		int previous= 0;
-		int current= text.indexOf(c, previous);
-
-		if (current == -1)
-			return text;
-
-		StringBuffer buffer= new StringBuffer();
-		while (current > -1) {
-			buffer.append(text.substring(previous, current));
-			buffer.append(s);
-			previous= current + 1;
-			current= text.indexOf(c, previous);
-		}
-		buffer.append(text.substring(previous));
-
-		return buffer.toString();
-	}
-
-	public static String convertToHTMLContent(String content) {
-		content= replace(content, '&', "&amp;"); //$NON-NLS-1$
-		content= replace(content, '"', "&quot;"); //$NON-NLS-1$
-		content= replace(content, '<', "&lt;"); //$NON-NLS-1$
-		return replace(content, '>', "&gt;"); //$NON-NLS-1$
-	}
-
-	public static String read(Reader rd) {
-
-		StringBuffer buffer= new StringBuffer();
-		char[] readBuffer= new char[2048];
-
-		try {
-			int n= rd.read(readBuffer);
-			while (n > 0) {
-				buffer.append(readBuffer, 0, n);
-				n= rd.read(readBuffer);
-			}
-			return buffer.toString();
-		} catch (IOException x) {
-		}
-
-		return null;
-	}
-
+	
 	public static void insertPageProlog(StringBuffer buffer, int position, RGB bgRGB, URL styleSheetURL) {
 
 		if (bgRGB == null)
@@ -131,25 +86,6 @@ public class HTMLPrinter {
 			
 			buffer.insert(position,  pageProlog.toString());
 		}
-	}
-
-	public static void insertStyles(StringBuffer buffer, String[] styles) {
-		if (styles == null || styles.length == 0)
-			return;
-
-		StringBuffer styleBuf= new StringBuffer(10 * styles.length);
-		for (int i= 0; styles != null && i < styles.length; i++) {
-			styleBuf.append(" style=\""); //$NON-NLS-1$
-			styleBuf.append(styles[i]);
-			styleBuf.append('"');
-		}
-
-		// Find insertion index
-		int index= buffer.indexOf("<body "); //$NON-NLS-1$
-		if (index == -1)
-			return;
-
-		buffer.insert(index+5, styleBuf);
 	}
 
 	public static void insertPageProlog(StringBuffer buffer, int position, RGB bgRGB) {
@@ -223,44 +159,9 @@ public class HTMLPrinter {
 	public static void addPageProlog(StringBuffer buffer) {
 		insertPageProlog(buffer, buffer.length());
 	}
-
+	
 	public static void addPageEpilog(StringBuffer buffer) {
 		buffer.append("</font></body></html>"); //$NON-NLS-1$
 	}
-
-	public static void startBulletList(StringBuffer buffer) {
-		buffer.append("<ul>"); //$NON-NLS-1$
-	}
-
-	public static void endBulletList(StringBuffer buffer) {
-		buffer.append("</ul>"); //$NON-NLS-1$
-	}
-
-	public static void addBullet(StringBuffer buffer, String bullet) {
-		if (bullet != null) {
-			buffer.append("<li>"); //$NON-NLS-1$
-			buffer.append(bullet);
-			buffer.append("</li>"); //$NON-NLS-1$
-		}
-	}
-
-	public static void addSmallHeader(StringBuffer buffer, String header) {
-		if (header != null) {
-			buffer.append("<h5>"); //$NON-NLS-1$
-			buffer.append(header);
-			buffer.append("</h5>"); //$NON-NLS-1$
-		}
-	}
-
-	public static void addParagraph(StringBuffer buffer, String paragraph) {
-		if (paragraph != null) {
-			buffer.append("<p>"); //$NON-NLS-1$
-			buffer.append(paragraph);
-		}
-	}
-
-	public static void addParagraph(StringBuffer buffer, Reader paragraphReader) {
-		if (paragraphReader != null)
-			addParagraph(buffer, read(paragraphReader));
-	}
+	
 }
