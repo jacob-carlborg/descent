fe8d96ee285196142242fb65ab16bf42287e9a0f
phoenix <phoenix@6538119e-2221-0410-ba12-9a5a7e5062c7>
2010-07-01 17:11:18 +0000

    scratchpad stuff
    git-svn-id: http://svn.dsource.org/projects/descent/trunk@1635 6538119e-2221-0410-ba12-9a5a7e5062c7

---
+++ ScratchPad/src/scratch/ScratchpadAPITest.java
@@ -0,0 +1,11 @@
+package scratch;
+
+import foo.Xpto;
+
+public class ScratchpadAPITest {
+	
+	
+	public static Xpto getSomeXpto() {
+		return new Xpto();
+	}
+}
---
+++ ScratchpadEclipse/src/scratchpad/APIUsageTest.java
@@ -0,0 +1,13 @@
+package scratchpad;
+
+import foo.Xpto;
+import scratch.ScratchpadAPITest;
+
+public class APIUsageTest {
+	
+	public static void main(String[] args) {
+		
+		Xpto someXpto = ScratchpadAPITest.getSomeXpto();
+	}
+	
+}
